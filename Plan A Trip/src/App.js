3f4dfe20acaccb02d92a5e913d70bb48c4e91461
Kshatriya Jyotiraditya Singh <kshatriyajyotiradityasingh@gmail.com>
2024-07-02 21:53:21 +0530

    Plan A trip completed

---
+++ Plan A Trip/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import data from "./data";
+import Tours from "./Components/Tours";
+import { useState } from "react";
+
+const App = () => {
+  const [tours, setTours] = useState(data);
+
+
+  function removeTour(id){
+    const newTours = tours.filter( tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+ 
+  if(tours.length === 0){
+    return(
+      <div className="refresh">
+        <h2>Sorry 😔😔...... <br/>No More Tours Left</h2>
+        <button onClick={() => setTours(data)} className="btn-white">Refresh</button>
+      </div>
+    )
+  }
+  return (
+    <div className="App">
+      <Tours tours={tours} removeTour={removeTour}> </Tours>
+    </div>
+  );
+};
+
+export default App;
